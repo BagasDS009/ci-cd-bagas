8d62af81a67997035a8be428b40ec8e44de17d7b
Bagas Dwi Saputra <bagas.dwi19008@student.unsika.ac.id>
2022-04-20 19:37:15 +0700

    menambahakan file

---
+++ src/test/java/starter/stepdefinitions/UserSteps.java
@@ -0,0 +1,30 @@
+package starter.stepdefinitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import net.thucydides.core.annotations.Steps;
+import starter.user.GetUser;
+
+public class UserSteps {
+    @Steps
+    GetUser getUser;
+
+    @Given("I set GET api endpoints")
+    public void setGetApiEndpoints(){
+        getUser.setApiEndpoint();
+    }
+    @When("I send GET HTTP request")
+    public void sendGetHttpRequest(){
+        getUser.sendGetHttpRequests();
+    }
+    @Then("I receive valid HTTP response code 200")
+    public void receiveValidHttpResponseCode(){
+        getUser.receiveValidfHttpResponseCode200();
+    }
+    @And("I receive valid data for detail user")
+    public void receiveValidDataForDetailUser(){
+        getUser.receiveValiDataForDetailUser();
+    }
+}
---
+++ src/test/java/starter/user/GetUser.java
@@ -0,0 +1,32 @@
+package starter.user;
+
+import net.serenitybdd.rest.SerenityRest;
+import net.thucydides.core.annotations.Step;
+
+import static net.serenitybdd.rest.SerenityRest.restAssuredThat;
+import static org.hamcrest.Matchers.equalTo;
+
+public class GetUser {
+    protected static String url = "https://request.in/api/";
+
+    @Step("I set GET api endpoints")
+    public String setApiEndpoint(){
+        return url + "users/2";
+    }
+
+    @Step("I send GET HTTP request")
+    public void sendGetHttpRequests(){
+        SerenityRest.given().get(setApiEndpoint());
+    }
+
+    @Step("I receive valid HTTP response code 200")
+    public void receiveValidfHttpResponseCode200(){
+        restAssuredThat(response -> response.statusCode(200));
+    }
+    @Step("I receive valid data for detail user")
+    public void receiveValiDataForDetailUser(){
+        restAssuredThat(response -> response.body("'data'.'id'", equalTo(2)));
+        restAssuredThat(response -> response.body("'data'.'first_name'", equalTo("Janet")));
+        restAssuredThat(response -> response.body("'data'.'last_name'", equalTo("Weaver")));
+    }
+}
